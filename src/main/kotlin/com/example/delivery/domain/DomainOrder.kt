25a48e2b49dd2f04223655e2a166ec3ef4b754cf
ajax-torbin-v <torbin.v@ajax.systems>
2024-08-21 10:48:38 +0300

    Added Domain models Moved Mongo models to separate package

---
+++ src/main/kotlin/com/example/delivery/domain/DomainOrder.kt
@@ -0,0 +1,14 @@
+package com.example.delivery.domain
+
+import com.example.delivery.mongo.MongoOrder.MongoShipmentDetails
+import org.bson.types.ObjectId
+import java.math.BigDecimal
+
+data class DomainOrder(
+    val id: ObjectId? = null,
+    val items: Map<ObjectId, Int>,
+    val totalPrice: BigDecimal,
+    val shipmentDetails: MongoShipmentDetails,
+    val status: String,
+    val userId: ObjectId?,
+)
---
+++ src/main/kotlin/com/example/delivery/mongo/MongoOrder.kt
@@ -0,0 +1,41 @@
+package com.example.delivery.mongo
+
+import org.bson.types.ObjectId
+import org.springframework.data.annotation.Id
+import org.springframework.data.annotation.TypeAlias
+import org.springframework.data.mongodb.core.mapping.Document
+import java.math.BigDecimal
+
+@TypeAlias("Order")
+@Document(collection = MongoOrder.COLLECTION_NAME)
+data class MongoOrder(
+    @Id val id: ObjectId? = null,
+    val items: Map<ObjectId, Int>? = null,
+    val totalPrice: BigDecimal? = null,
+    val shipmentDetails: MongoShipmentDetails? = null,
+    val status: Status? = null,
+    val userId: ObjectId? = null,
+) {
+    enum class Status {
+        NEW,
+        SHIPPING,
+        COMPLETED,
+        CANCELED;
+
+        fun fromValue(value: String): Status? {
+            return entries.find { it.name.equals(this.name, ignoreCase = true) }
+        }
+    }
+
+    companion object {
+        const val COLLECTION_NAME = "order"
+    }
+
+    @TypeAlias("ShipmentDetails")
+    data class MongoShipmentDetails(
+        val city: String? = null,
+        val street: String? = null,
+        val building: String? = null,
+        val index: String? = null,
+    )
+}
