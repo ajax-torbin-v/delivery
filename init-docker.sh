9e72b56e7c31832be7f237b47cb3ad270214440c
ajax-torbin-v <torbin.v@ajax.systems>
2024-09-10 15:22:31 +0300

    Fixed script to run docker compose Added volume for data persistence

--- init-docker.sh
+++ init-docker.sh
@@ -1,34 +1,51 @@
 #!/bin/bash
 
-while [[ "$#" -gt 0 ]]; do
-    case "$1" in
-        --gen-env | -ge)
-            if [ -f ".env" ]; then
-                awk -F'=' '/^[^#]/ { print $1"=#" }' .env > .env_sample
-            fi
-            ;;
+declare -A flag_map
 
-        --help | -h)
-            echo "Pls refer to https://tinyurl.com/5br5nhmy"
-            exit 1
-            ;;
-        *)
-            echo "Unknown flag: $1"
-            exit 1
-            ;;
-    esac
+while [[ $# -gt 0 ]]; do
+    key="$1"
     shift
+
+    if [[ $key == -* ]]; then
+        value_list=()
+        while [[ $# -gt 0 && $1 != -* ]]; do
+            value_list+=("$1")
+            shift
+        done
+        flag_map["$key"]="${value_list[*]}"
+    fi
 done
 
-./gradlew -q check > output.log 2>&1
+help () {
+  cat << EOF
+  usage: init-docker [-h | --help][-ge | --gen-env]
+  Global options
+  -ge, --gen--env : Generates .env_sample file based on .env
+  -h,  --help : Print help message
+  Report bugs to: https://github.com/ajax-torbin-v
+  You can find more about this script https://tinyurl.com/5br5nhmy
+EOF
+}
 
-if grep -q "BUILD FAILED" output.log; then
-    echo "Check task failed!"
-    cat output.log
-    rm output.log
-    exit
-fi
+for key in "${!flag_map[@]}"
+do
+  case $key in
+  -h | --help)
+    help
+    exit 0
+    ;;
+  -ge | --gen-env)
 
-./gradlew assemble
+    awk -F'=' '/^[^#]/ { print $1"=#" }' .env > .env_sample
+    exit 0
+    ;;
+  *)
+    echo "Unknown argument ${flag_map[key]}"
+    exit 1
+    ;;
+  esac
+done
 
-docker-compose up
+./gradlew clean build
+
+docker-compose up -d --build --force-recreate --remove-orphans
