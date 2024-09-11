0e5135203687e123784e4322a596e70ac7f34521
ajax-torbin-v <torbin.v@ajax.systems>
2024-09-11 12:41:36 +0300

    Fixed issue

--- start-minikube.sh
+++ start-minikube.sh
@@ -10,12 +10,9 @@ if minikube status | grep -q "Running"; then
     echo "Minikube is already running."
 else
     MINIKUBE_DRIVER=${1:-docker}
-
     echo "Starting Minikube with driver: $MINIKUBE_DRIVER..."
-    minikube start --driver="$MINIKUBE_DRIVER"
-
-    if [ $? -eq 0 ]; then
-        echo "Minikube started successfully."
+    if minikube start --driver="$MINIKUBE_DRIVER"
+        then echo "Minikube started successfully."
     else
         echo "Failed to start Minikube. Please check logs for more information."
         exit 1
@@ -31,7 +28,7 @@ minikube addons enable ingress
 docker build -t delivery:latest .
 
 kubectl apply -f k8s/config/mongo-secret.yaml
-kubectl apply -f k8s/config/mongo-configmap.yaml
+kubectl apply -f k8s/config/mongo-express-configmap.yaml
 kubectl apply -f k8s/config/delivery-configmap.yaml
 kubectl apply -f k8s/config/mongo-deployment.yaml
 
@@ -42,5 +39,3 @@ kubectl apply -f k8s/config/delivery-deployment.yaml
 kubectl apply -f k8s/config/ingress.yaml
 
 kubectl wait --for=condition=ready pod --all --timeout=300s
-
-minikube service mongo-express-service --url
--- stop-minikube.sh
+++ stop-minikube.sh
@@ -1,9 +1,2 @@
 #!/bin/bash
-kubectl delete -f k8s/config/mongo-configmap.yaml
-kubectl delete -f k8s/config/mongo-deployment.yaml
-kubectl delete -f k8s/config/mongo-secret.yaml
-kubectl delete -f k8s/config/delivery-deployment.yaml
-kubectl delete -f k8s/config/mongo-express-deployment.yaml
-kubectl delete -f k8s/config/ingress.yaml
-
-minikube addons disable ingress
+kubectl delete -f k8s/config/
