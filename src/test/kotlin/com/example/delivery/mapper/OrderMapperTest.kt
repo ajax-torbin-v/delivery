3ab45df78e4d03d1e7122065ac319b92b05a1bb5
ajax-torbin-v <torbin.v@ajax.systems>
2024-09-23 13:54:51 +0300

    Fixed deltaCoverage

--- src/test/kotlin/com/example/delivery/mapper/OrderMapperTest.kt
+++ src/test/kotlin/com/example/delivery/mapper/OrderMapperTest.kt
@@ -3,6 +3,7 @@ package com.example.delivery.mapper
 import com.example.delivery.domain.DomainOrder
 import com.example.delivery.mapper.OrderMapper.toDomain
 import com.example.delivery.mongo.MongoOrder
+import org.bson.types.ObjectId
 import org.junit.jupiter.api.Test
 import kotlin.test.assertEquals
 
@@ -11,12 +12,46 @@ class OrderMapperTest {
     fun `mapper should handle nullable fields of shipment details`() {
         // GIVEN
         val mongoShipmentDetails = MongoOrder.MongoShipmentDetails()
-        val domainShipmentDetails = DomainOrder.DomainShipmentDetails("", "", "", "")
+        val domainShipmentDetails = DomainOrder.DomainShipmentDetails()
 
         // WHEN
         val actual = mongoShipmentDetails.toDomain()
 
         // THEN
         assertEquals(domainShipmentDetails, actual)
+    }
+
+    @Test
+    fun `mapper should handle nullable fields of order item`() {
+        // GIVEN
+        val id = ObjectId()
+        val mongoOrderItem = MongoOrder.MongoOrderItem(productId = id)
+        val domainOrderItem = DomainOrder.DomainOrderItem(productId = id)
+
+        // WHEN
+        val actual = mongoOrderItem.toDomain()
+
+        // THEN
+        assertEquals(domainOrderItem, actual)
+    }
+
+    @Test
+    fun `mapper should handle nullable fields of order`() {
+        // GIVEN
+        val id = ObjectId()
+        val mongoOrder = MongoOrder(id = id, userId = id)
+        val domainOrder = DomainOrder(
+            id = id,
+            emptyList(),
+            DomainOrder.DomainShipmentDetails(),
+            DomainOrder.Status.UNKNOWN,
+            userId = id
+        )
+
+        // WHEN
+        val actual = mongoOrder.toDomain()
+
+        // THEN
+        assertEquals(domainOrder, actual)
     }
 }
