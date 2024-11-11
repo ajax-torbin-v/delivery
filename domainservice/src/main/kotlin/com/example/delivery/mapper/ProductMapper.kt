69495555354008e6765fabe136dd44b754382781
ajax-torbin-v <torbin.v@ajax.systems>
2024-11-11 15:12:17 +0200

    Test fixes

--- domainservice/src/main/kotlin/com/example/delivery/mapper/ProductMapper.kt
+++ domainservice/src/main/kotlin/com/example/delivery/mapper/ProductMapper.kt
@@ -4,7 +4,6 @@ import com.example.core.dto.request.CreateProductDTO
 import com.example.core.dto.request.UpdateProductDTO
 import com.example.delivery.domain.DomainProduct
 import com.example.delivery.mongo.MongoProduct
-import org.springframework.data.mongodb.core.query.Update
 import java.math.BigDecimal
 
 object ProductMapper {
@@ -33,14 +32,5 @@ object ProductMapper {
             amountAvailable = updateProductDTO.amountAvailable ?: amountAvailable,
             measurement = updateProductDTO.measurement ?: measurement
         )
-    }
-
-    fun UpdateProductDTO.toUpdate(): Update {
-        val update = Update()
-        name?.let { update.set(MongoProduct::name.name, it) }
-        price?.let { update.set(MongoProduct::price.name, it) }
-        amountAvailable?.let { update.set(MongoProduct::amountAvailable.name, it) }
-        measurement?.let { update.set(MongoProduct::measurement.name, it) }
-        return update
     }
 }
