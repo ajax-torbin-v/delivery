a24f263cfaeb9e992f750ee705e0d0d504b3d1ce
ajax-torbin-v <torbin.v@ajax.systems>
2024-11-12 13:30:45 +0200

    Incredibly small fix

--- domainservice/src/main/kotlin/com/example/delivery/config/RedisProperties.kt
+++ domainservice/src/main/kotlin/com/example/delivery/config/RedisProperties.kt
@@ -3,7 +3,7 @@ package com.example.delivery.config
 import org.springframework.boot.context.properties.ConfigurationProperties
 
 @ConfigurationProperties(prefix = "spring.redis")
-class RedisProperties(
+data class RedisProperties(
     val host: String,
     val port: Int,
     val timeout: Long,
