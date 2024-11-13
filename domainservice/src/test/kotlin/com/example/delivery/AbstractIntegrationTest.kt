618c0867e598ba940c8c4fbf8c99bf6e63464fbb
ajax-torbin-v <torbin.v@ajax.systems>
2024-11-13 16:34:08 +0200

    Added needed beans for testing

--- domainservice/src/test/kotlin/com/example/delivery/AbstractIntegrationTest.kt
+++ domainservice/src/test/kotlin/com/example/delivery/AbstractIntegrationTest.kt
@@ -1,34 +1,33 @@
 package com.example.delivery
 
-import com.example.delivery.kafka.KafkaTest.Companion.NOTIFICATION_GROUP
-import com.example.delivery.kafka.configuration.KafkaConfiguration
-import com.example.internal.api.KafkaTopic
-import org.springframework.beans.factory.annotation.Value
+import org.apache.kafka.clients.admin.Admin
+import org.apache.kafka.clients.admin.KafkaAdminClient
+import org.apache.kafka.common.serialization.ByteArrayDeserializer
+import org.apache.kafka.common.serialization.StringDeserializer
 import org.springframework.boot.autoconfigure.kafka.KafkaProperties
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Import
+import org.springframework.kafka.core.ConsumerFactory
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory
+import org.springframework.kafka.core.KafkaAdmin
 import org.springframework.test.context.ActiveProfiles
-import reactor.kafka.receiver.KafkaReceiver
 
 @SpringBootTest
 @ActiveProfiles("test")
 @Import(AbstractIntegrationTest.MyKafkaTestConfiguration::class)
 abstract class AbstractIntegrationTest {
-    class MyKafkaTestConfiguration(
-        @Value("\${spring.kafka.bootstrap-servers}") val bootstrapServer: String,
-        kafkaProperties: KafkaProperties,
-    ) : KafkaConfiguration(
-        bootstrapServer,
-        kafkaProperties
-    ) {
+    class MyKafkaTestConfiguration {
         @Bean
-        fun kafkaTestReceiver(): KafkaReceiver<String, ByteArray> {
-            return createKafkaReceiver(
-                baseConsumerProperties(),
-                KafkaTopic.KafkaOrderStatusUpdateEvents.NOTIFICATIONS,
-                NOTIFICATION_GROUP
+        fun adminClient(kafkaAdmin: KafkaAdmin): Admin =
+            KafkaAdminClient.create(kafkaAdmin.configurationProperties)
+
+        @Bean
+        fun consumerFactory(kafkaProperties: KafkaProperties): ConsumerFactory<String, ByteArray> =
+            DefaultKafkaConsumerFactory(
+                kafkaProperties.buildConsumerProperties(null),
+                StringDeserializer(),
+                ByteArrayDeserializer()
             )
-        }
     }
 }
